83485ad35b82199325ca24d20b5b096a6de9300e
Kira9112 <kira91_911@mail.ru>
2021-06-20 20:21:16 +0300

    Reformate code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int mass, double height) {
-        double index = mass /(height*height) ;
+        double index = mass / (height * height);
         return (int) index;
     }
 }
